2efd1203c75878c4440263b6aa0eace8747861cb
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-02-24 22:24:02 +0000

    implemented the average rating feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def average_rating(product)
+    average_rating = 0
+    reviews =  Review.where(product_id: product.id)
+    if reviews.length > 0
+      average_rating = Review.where(product_id: product.id).average(:rating).round(2)
+    end
+    average_rating
+  end
 end
